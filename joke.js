8ac2593aaa7bbbd9305ecf8a36e67f53f8aa7658
HamoudyAwada <Mohamad.Awada04@outlook.com>
2025-03-25 18:49:39 -0600

    complete the rest of the buttons to pull and generate jokes from jokeapi

--- joke.js
+++ joke.js
@@ -20,15 +20,69 @@ const button1 = document.getElementById("misc")
 const button2 = document.getElementById("programming")
 const button3 = document.getElementById("spooky")
 const button4 = document.getElementById("christmas")
+const button5 = document.getElementById("pun")
 
 button1.textContent = "Miscelaneous"
 button2.textContent = "Programming"
 button3.textContent = "Spooky"
 button4.textContent = "Christmas"
+button5.textContent = "Pun"
 
 button1.addEventListener("click", function(){
 
-    fetch("https://v2.jokeapi.dev/joke/Misc?safe-mode")
+    fetch("https://v2.jokeapi.dev/joke/misc?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.type === "twopart"){
+            start.textContent = data.setup + " ... " + data.delivery
+        } else if (data.type === "single"){
+            start.textContent = data.joke
+        }
+})})
+
+button2.addEventListener("click", function(){
+
+    fetch("https://v2.jokeapi.dev/joke/programming?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.type === "twopart"){
+            start.textContent = data.setup + " ... " + data.delivery
+        } else if (data.type === "single"){
+            start.textContent = data.joke
+        }
+})})
+
+button3.addEventListener("click", function(){
+
+    fetch("https://v2.jokeapi.dev/joke/spooky?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.type === "twopart"){
+            start.textContent = data.setup + " ... " + data.delivery
+        } else if (data.type === "single"){
+            start.textContent = data.joke
+        }
+})})
+
+button4.addEventListener("click", function(){
+
+    fetch("https://v2.jokeapi.dev/joke/christmas?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.type === "twopart"){
+            start.textContent = data.setup + " ... " + data.delivery
+        } else if (data.type === "single"){
+            start.textContent = data.joke
+        }
+})})
+
+button5.addEventListener("click", function(){
+
+    fetch("https://v2.jokeapi.dev/joke/pun?safe-mode")
     .then(response => response.json())
     .then(data => {
 
